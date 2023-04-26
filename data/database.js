924b6cf6a294c8bc67ca0e372ca151d60ccfc612
Swapnil07 <sonawaneswapnil077@gmail.com>
2023-04-26 10:44:22 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -8,6 +8,6 @@ export const connectDB = () => {
         dbName: "backendapi",
       }
     )
-    .then(() => console.log("database connected"))
+    .then((c) => console.log(`database connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
